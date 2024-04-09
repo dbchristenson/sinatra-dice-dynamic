d41bc290b868b3781a9f99d4a225e9326d3203f8
DB Christenson <dbeauchristenson@gmail.com>
2024-04-09 23:18:18 +0000

    Added dynamic urls for dice

--- app.rb
+++ app.rb
@@ -5,6 +5,20 @@ get("/") do
   erb(:homepage)
 end
 
+get("/dynamic/:number_of_dice/:number_of_sides") do
+  @number_of_dice = params.fetch("number_of_dice").to_i
+  @number_of_sides = params.fetch("number_of_sides").to_i
+  @rolls = []
+
+  @number_of_dice do
+    die = rand(1..@number_of_sides)
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
+end
+
+
 get("/dice/2/6") do
   @rolls = []
 
